7c06d040f77ea3c72ad5741d540b06f47607a201
Joe <jgalante00@gmail.com>
2021-07-12 15:06:41 -0400

    contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,30 @@
 // imports
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 // contexts
 import { NavContext } from '../contexts/NavContext';
+import { UserContext } from '../contexts/UserContext';
 
 const Contact = () =>
 {
     // contexts
     const { navState } = useContext(NavContext);
     const [ nav, setNav ] = navState;
+    const { userState } = useContext(UserContext);
+    const [ user ] = userState;
+
+    // states
+    const [ contacting, setContacting ] = useState(false);
+    const [ phone, setPhone ] = useState('');
+    const [ subject, setSubject ] = useState('');
+    const [ contactMessage, setContactMessage ] = useState('');
+
+    // functions
+    const dropLine = (e) =>
+    {
+        e.preventDefault();
+
+    }
 
     // on component load
     useEffect(() => {setNav("#contact")}, []);
@@ -51,7 +67,43 @@ const Contact = () =>
             <p>Wilton Manors, FL 33311, USA</p>
             <p>*Multiple Locations</p>
             
-            <input type="button" value="Contact Us" />
+            <input type="button" value="Contact Us" onClick={() => {setContacting(true)}} />
+
+            {contacting ?
+                <section className="drop-line">
+                    <form className="contact-form" onSubmit={dropLine}>
+                        <label className="name-label">
+                            <span>Name</span>
+                            <span className="required">*</span>
+                        </label>
+                        <input id="name" className="contact-input" type="text" placeholder="Name" value={user.name} />
+                        <label className="email-label">
+                            <span>Email</span>
+                            <span className="required">*</span>
+                        </label>
+                        <input id="email" className="contact-input" type="text" placeholder="Email" value={user.email} />
+                        <label className="phone-label">
+                            <span>Phone</span>
+                            <span className="required">*</span>
+                        </label>
+                        <input id="phone" className="contact-input" type="text" placeholder="Phone" value={phone} onChange={(e) => {setPhone(e.target.value)}} />
+                        <label className="subject-label">
+                            <span>Subject</span>
+                            <span className="required">*</span>
+                        </label>
+                        <input id="subject" className="contact-input" type="text" placeholder="Subject" value={subject} onChange={(e) => {setSubject(e.target.value)}} />
+                        <label className="message-label">
+                            <span>Message</span>
+                            <span className="required">*</span>
+                        </label>
+                        <input id="message" className="contact-input" type="text" placeholder="Message" value={contactMessage} onChange={(e) => {setContactMessage(e.target.value)}} />
+                    </form>
+                    <input type="button" value="Cancel" onClick={() => {setContacting(false)}} />
+                    <input type="submit" value="Submit" onClick={dropLine} />
+                </section>
+                :
+                null
+            }
         </div>
     )
 }
