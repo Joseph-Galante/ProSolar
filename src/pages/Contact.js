8d7594012065ecdb7e2bc0961a1acef00c8639d6
Joe <jgalante00@gmail.com>
2021-07-12 15:57:13 -0400

    contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 // imports
 import { useContext, useEffect, useState } from 'react';
+import { send } from 'emailjs-com';
 
 // contexts
 import { NavContext } from '../contexts/NavContext';
@@ -23,7 +24,71 @@ const Contact = () =>
     const dropLine = (e) =>
     {
         e.preventDefault();
-
+        // clear messages
+        document.querySelector(".messages").innerHTML = null;
+        // check for empty fields
+        if (phone === "" || subject === "" || contactMessage === "")
+        {
+            if (phone === "")
+            {
+                // grab phone input
+                const phoneInput = document.querySelector("#phone");
+                // add invalid class
+                phoneInput.classList.add("invalid");
+                // create invalid message
+                const message = document.createElement("p");
+                message.innerHTML = "Phone is required";
+                message.style.color = "red";
+                // add message
+                document.querySelector(".messages").append(message);
+            }
+            else
+            {
+                document.querySelector("#phone").classList.remove("invalid");
+            }
+            if (subject === "")
+            {
+                // grab subject input
+                const subjectInput = document.querySelector("#subject");
+                // add invalid class
+                subjectInput.classList.add("invalid");
+                // create invalid message
+                const message = document.createElement("p");
+                message.innerHTML = "Subject is required";
+                message.style.color = "red";
+                // add message
+                document.querySelector(".messages").append(message);
+            }
+            else
+            {
+                document.querySelector("#subject").classList.remove("invalid");
+            }
+            if (contactMessage === "")
+            {
+                // grab message input
+                const messageInput = document.querySelector("#message");
+                // add invalid class
+                messageInput.classList.add("invalid");
+                // create invalid message
+                const message = document.createElement("p");
+                message.innerHTML = "Message is required";
+                message.style.color = "red";
+                // add message
+                document.querySelector(".messages").append(message);
+            }
+            else
+            {
+                document.querySelector("#message").classList.remove("invalid");
+            }
+            return;
+        }
+        else
+        {
+            document.querySelectorAll(".contact-input").forEach(i =>
+            { 
+                i.classList.remove("invalid");
+            });
+        }
     }
 
     // on component load
@@ -31,57 +96,59 @@ const Contact = () =>
 
     return (
         <div className="contact-page">
-            <h3>HEADQUARTERS</h3>
-            <h3>US VIRGIN ISLANDS</h3>
-            <br/>
+            <section className={contacting ? "contact-info blurred" : "contact-info"}>
+                <h3 style={{margin: "20px 0 10px 0"}}>HEADQUARTERS</h3>
+                <h3 style={{margin: "10px 0"}}>US VIRGIN ISLANDS</h3>
 
-            <h3>ST. THOMAS</h3>
-            <p>(340) 201 4752</p>
-            <p>(Across from Home Depot)</p>
-            <p>Bottom of Donoe Hill</p>
-            <p>4011 New Herrnhut Suite 200</p>
-            <p>St. Thomas VI, 00802</p>
+                <h3>ST. THOMAS</h3>
+                <p>(340) 201 4752</p>
+                <p>(Across from Home Depot)</p>
+                <p>Bottom of Donoe Hill</p>
+                <p>4011 New Herrnhut Suite 200</p>
+                <p>St. Thomas VI, 00802</p>
 
-            <h3>ST. CROIX</h3>
-            <p>(340) 514 1530</p>
-            <p>(Behind Maria's Cantina)</p>
-            <p>Gallows Bay</p>
-            <p>5007 Starboard St, Suite 7</p>
-            <p>Christiansted, 00820</p>
+                <h3>ST. CROIX</h3>
+                <p>(340) 514 1530</p>
+                <p>(Behind Maria's Cantina)</p>
+                <p>Gallows Bay</p>
+                <p>5007 Starboard St, Suite 7</p>
+                <p>Christiansted, 00820</p>
 
-            <h3>BRITISH VIRGIN ISLANDS</h3>
-            <p>(284) 494 1478</p>
-            <p>Port Purcell Warehouse 12</p>
-            <p>Roadtown, Tortola VG1110</p>
-            <p>British Virgin Islands</p>
+                <h3>BRITISH VIRGIN ISLANDS</h3>
+                <p>(284) 494 1478</p>
+                <p>Port Purcell Warehouse 12</p>
+                <p>Roadtown, Tortola VG1110</p>
+                <p>British Virgin Islands</p>
 
-            <h3>CAYMAN ISLANDS</h3>
-            <p>(345) 926 2676</p>
-            <p>5 Blue Bird Warehouse</p>
-            <p>Cane Bird Avenue</p>
-            <p>Cayman Islands</p>
+                <h3>CAYMAN ISLANDS</h3>
+                <p>(345) 926 2676</p>
+                <p>5 Blue Bird Warehouse</p>
+                <p>Cane Bird Avenue</p>
+                <p>Cayman Islands</p>
 
-            <h3>FLORIDA</h3>
-            <p>1 (888) 462 5005</p>
-            <p>1100 W Oakland Park Blvd Unit 8</p>
-            <p>Wilton Manors, FL 33311, USA</p>
-            <p>*Multiple Locations</p>
-            
-            <input type="button" value="Contact Us" onClick={() => {setContacting(true)}} />
+                <h3>FLORIDA</h3>
+                <p>1 (888) 462 5005</p>
+                <p>1100 W Oakland Park Blvd Unit 8</p>
+                <p>Wilton Manors, FL 33311, USA</p>
+                <p>*Multiple Locations</p>
+                
+                <input type="button" value="Contact Us" onClick={() => {setContacting(true)}} />
+            </section>
 
             {contacting ?
                 <section className="drop-line">
+                    <h3>Drop us a line</h3>
                     <form className="contact-form" onSubmit={dropLine}>
                         <label className="name-label">
                             <span>Name</span>
                             <span className="required">*</span>
                         </label>
-                        <input id="name" className="contact-input" type="text" placeholder="Name" value={user.name} />
+                        <input id="name" className="contact-input" type="text" placeholder="Name" value={user.name}  readOnly />
                         <label className="email-label">
                             <span>Email</span>
                             <span className="required">*</span>
                         </label>
-                        <input id="email" className="contact-input" type="text" placeholder="Email" value={user.email} />
+                        <input id="email" className="contact-input" type="text" placeholder="Email" value={user.email}  readOnly />
                         <label className="phone-label">
                             <span>Phone</span>
                             <span className="required">*</span>
@@ -96,10 +163,12 @@ const Contact = () =>
                             <span>Message</span>
                             <span className="required">*</span>
                         </label>
-                        <input id="message" className="contact-input" type="text" placeholder="Message" value={contactMessage} onChange={(e) => {setContactMessage(e.target.value)}} />
+                        <textarea id="message" className="contact-input" type="text" placeholder="Message" value={contactMessage} onChange={(e) => {setContactMessage(e.target.value)}} />
                     </form>
-                    <input type="button" value="Cancel" onClick={() => {setContacting(false)}} />
-                    <input type="submit" value="Submit" onClick={dropLine} />
+                    <section className="contact-inputs">
+                        <input type="button" value="Cancel" onClick={() => {setContacting(false)}} />
+                        <input type="submit" value="Submit" onClick={dropLine} />
+                    </section>
                 </section>
                 :
                 null
